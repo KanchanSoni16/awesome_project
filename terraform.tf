2ab6ae3fecc060973b00a8e8d7f0ab4bc6c26b44
Kanchan Soni <kanchansoni.cse@gmail.com>
2022-12-27 19:28:55 +0530

    Pushing code in github repo

---
+++ terraform.tf
@@ -0,0 +1,12 @@
+
+# terraform backend creation
+/*
+terraform{
+  backend "s3" {
+    bucket = "statefile-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "state-locking"
+  }
+}*/
+
