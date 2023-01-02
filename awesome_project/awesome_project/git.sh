4243fe1717ccdd4613a6165064e2c5547388d978
Kanchan Soni <kanchansoni.cse@gmail.com>
2023-01-02 14:16:34 +0530

    Pushing code in github repo

---
+++ awesome_project/awesome_project/git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+$tokens = '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'
+git clone https://github.com/KanchanSoni16/awesome_project.git
+cp -r * ./awesome_project
+cd awesome_project
+#git switch master
+#git diff master origin/master
+git add .
+git commit -m "Pushing code in github repo"
+git push https://KanchanSoni16:$tokens@github.com/KanchanSoni16/awesome_project.git
