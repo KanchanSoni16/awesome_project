2905eb65008e1a284bd6a64d9d1b3931ab3053d5
Kanchan Soni <kanchansoni.cse@gmail.com>
2023-01-02 14:23:43 +0530

    Pushing code in github repo

--- git.sh
+++ git.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-$tokens = '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'
-git clone https://github.com/KanchanSoni16/awesome_project.git
-cp -r * ./awesome_project
+#$tokens = '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'
+
+#git clone https://github.com/KanchanSoni16/awesome_project.git
+
+git clone git@github.com:KanchanSoni16/awesome_project.git
+#rsync ./ ./awesome_project
+rsync -a --exclude={'awesome_project'} . ./awesome_project
 cd awesome_project
+#cp -r ../* ./awesome_project --exclude awesome_project
 #git switch master
 #git diff master origin/master
 git add .
 git commit -m "Pushing code in github repo"
-git push https://KanchanSoni16:$tokens@github.com/KanchanSoni16/awesome_project.git
+#git push https://KanchanSoni16:$tokens@github.com/KanchanSoni16/awesome_project.git
+
+git push git@github.com:KanchanSoni16/awesome_project.git
