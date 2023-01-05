7efd954cf9c8a369f2fe1d2dfd074025cde609cf
Kachan <kanchansoni.cse@gmail.com>
2023-01-05 12:44:14 +0530

    Pushing code in github repo

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone git@github.com:KanchanSoni16/awesome_project.git
-rsync -a --exclude 'awesome_project' --exclude '.terraform' ./ ./awesome_project/
+rsync -av --exclude 'awesome_project' --exclude '.terraform' ./ ./awesome_project/
 cd awesome_project
 git add .
 git commit -m "Pushing code in github repo"
